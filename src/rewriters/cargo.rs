abcce73f114bf5020b38fc71422c37f34dbcc9bd
Peter Williams <peter@newton.cx>
2020-08-25 23:40:58 -0400

    src/rewriters/cargo.rs: write out versions for internal deps!
    Messy, as expected, but it might even work!

--- src/rewriters/cargo.rs
+++ src/rewriters/cargo.rs
@@ -45,9 +45,21 @@ impl Rewriter for CargoRewriter {
         }
         let mut doc: Document = s.parse()?;
 
-        // Update the project version
+        // Helper table for applying internal deps. Note that we use the 0'th
+        // qname, not the user-facing name, since that is what is used in
+        // Cargo-land.
 
         let proj = app.graph().lookup(self.proj_id);
+        let mut internal_reqs = HashMap::new();
+
+        for req in &proj.internal_reqs[..] {
+            internal_reqs.insert(
+                app.graph().lookup(req.ident).qualified_names()[0].clone(),
+                req.min_version.clone(),
+            );
+        }
+
+        // Update the project version
 
         {
             let ct_root = doc.as_table_mut();
@@ -60,7 +72,62 @@ impl Rewriter for CargoRewriter {
 
             ct_package["version"] = toml_edit::value(proj.version.to_string());
 
-            // TODO: internal dependencies!!!
+            // Rewrite any internal dependencies. These may be found in three
+            // main tables and a nested table of potential target-specific
+            // tables.
+
+            for tblname in &["dependencies", "dev-dependencies", "build-dependencies"] {
+                if let Some(tbl) = ct_root.entry(tblname).as_table_mut() {
+                    rewrite_deptable(&internal_reqs, tbl)?;
+                }
+            }
+
+            if let Some(ct_target) = ct_root.entry("target").as_table_mut() {
+                // As far as I can tell, no way to iterate over the table while mutating
+                // its values?
+                let target_specs = ct_target
+                    .iter()
+                    .map(|(k, _v)| k.to_owned())
+                    .collect::<Vec<_>>();
+
+                for target_spec in &target_specs[..] {
+                    if let Some(tbl) = ct_target.entry(target_spec).as_table_mut() {
+                        rewrite_deptable(&internal_reqs, tbl)?;
+                    }
+                }
+            }
+        }
+
+        fn rewrite_deptable(
+            internal_reqs: &HashMap<String, Version>,
+            tbl: &mut toml_edit::Table,
+        ) -> Result<()> {
+            let deps = tbl.iter().map(|(k, _v)| k.to_owned()).collect::<Vec<_>>();
+
+            for dep in &deps[..] {
+                // ??? renamed internal deps? We could save rename informaion
+                // from cargo-metadata when we load everything.
+
+                if let Some(min_version) = internal_reqs.get(dep) {
+                    if let Some(dep_tbl) = tbl.entry(dep).as_table_mut() {
+                        dep_tbl["version"] = toml_edit::value(format!("^{}", min_version));
+                    } else if let Some(dep_tbl) = tbl.entry(dep).as_inline_table_mut() {
+                        // Can't just index inline tables???
+                        if let Some(val) = dep_tbl.get_mut("version") {
+                            *val = format!("^{}", min_version).into();
+                        } else {
+                            dep_tbl.get_or_insert("version", format!("^{}", min_version));
+                        }
+                    } else {
+                        return Err(Error::Environment(format!(
+                            "unexpected internal dependency item in a Cargo.toml: {:?}",
+                            tbl.entry(dep)
+                        )));
+                    }
+                }
+            }
+
+            Ok(())
         }
 
         // Rewrite.
