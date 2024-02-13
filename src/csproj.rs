ffaf26e3b06591d2a57d81da98b7495ac1f13b75
Peter Williams <peter@newton.cx>
2024-02-12 21:15:40 -0500

    Update uuid from 0.8.x to 1.x

--- src/csproj.rs
+++ src/csproj.rs
@@ -547,7 +547,7 @@ impl Rewriter for VdprojRewriter {
 
         let proj = app.graph().lookup(self.proj_id);
 
-        let mut pcode = uuid::Uuid::new_v4().to_hyphenated().to_string();
+        let mut pcode = uuid::Uuid::new_v4().hyphenated().to_string();
         pcode.make_ascii_uppercase();
         let pcode = format!("{{{}}}", pcode);
         info!(
